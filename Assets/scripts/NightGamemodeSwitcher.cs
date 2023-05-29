18275f80379f49c9cae2ef126e911621d5060407
wfelzera <crafty.radu@gmail.com>
2023-05-30 00:59:09 +0300

    made sum stuff
    -gamemode switcher
    -night mode harder than day

---
+++ Assets/scripts/NightGamemodeSwitcher.cs
@@ -0,0 +1,12 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class NightGamemodeSwitcher : MonoBehaviour
+{
+    private void OnMouseDown()
+    {
+        SceneManager.LoadScene("NightGameModeSwitcher");
+    }
+}
